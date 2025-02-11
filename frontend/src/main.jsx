01eee300a43f59573203991b999a33cf6f780fd4
Hugocl01 <hugocayon@gmail.com>
2025-02-11 03:03:48 +0100

    Envoltura del la raiz con el contexto

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,10 +6,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
+import { SeguridadProvider } from './contexts/SeguridadProvider.jsx';
 import AppEnrutador from './routers/AppEnrutador.jsx';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <AppEnrutador />
+        <SeguridadProvider>
+            <AppEnrutador />
+        </SeguridadProvider>
     </StrictMode>,
 )
