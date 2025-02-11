ae4d2e3ba8cf0d3b4e06b899bd7ca73016b1b7b3
Hugocl01 <hugocayon@gmail.com>
2025-02-11 12:29:21 +0100

    Pagina equipo y login

--- frontend/src/pages/EquiposPage.jsx
+++ frontend/src/pages/EquiposPage.jsx
@@ -1,5 +1,12 @@
+/**
+ * Página de Equipos.
+ *
+ * Esta página renderiza el componente de listado de equipos (`ListaEquipos`),
+ *
+ * @component
+ * @returns {JSX.Element} - La estructura de la página de equipos.
+ */
 function EquiposPage() {
-
     return (
         <>
             pagina de equipos
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,12 +1,20 @@
 import Login from "../components/Login.jsx";
 
+/**
+ * Página de Login.
+ *
+ * Esta página renderiza el componente de inicio de sesión (`Login`),
+ * permitiendo a los usuarios autenticarse en la aplicación.
+ *
+ * @component
+ * @returns {JSX.Element} - La estructura de la página de login.
+ */
 function LoginPage() {
-
     return (
         <>
             <Login />
         </>
-    )
+    );
 }
 
 export default LoginPage;
