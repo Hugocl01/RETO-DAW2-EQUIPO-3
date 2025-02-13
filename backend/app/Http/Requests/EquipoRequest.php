9aaadb78fb7af0c6c0d3ba696ab015bcafd8cd7f
DiegoFerF <diegoff630@gmail.com>
2025-02-13 17:04:23 +0100

    Creacion de JugadorRequest/JugadorResource y modificacion del EquipoRequest

--- backend/app/Http/Requests/EquipoRequest.php
+++ backend/app/Http/Requests/EquipoRequest.php
@@ -17,7 +17,14 @@ class EquipoRequest extends FormRequest
             'nombre' => 'required|string|max:255|unique:equipos,nombre',
             'centro_id' => 'required|exists:centros,id',
             'grupo' => 'required|string|max:1|in:A,B',
-            'usuario_id' => 'required|exists:usuarios,id'
+            'usuario_id' => 'required|exists:usuarios,id',
+            'jugadores.*.equipo_id' => 'required|int',
+            'jugadores.*.nombre_completo' => 'required|string|min:3|max:70',
+            'jugadores.*.capitan' => 'required|int',
+            'jugadores.*.estudio_id' => 'int',
+            'jugadores.*.dni' => 'string|min:9|max:9|digits:8|unique:jugadores,dni',
+            'jugadores.*.email' => 'string|email|unique:jugadores,email',
+            'jugadores.*.telefono' => 'string|min:9|max:9',
         ];
     }
 
@@ -36,6 +43,26 @@ class EquipoRequest extends FormRequest
             'grupo.in' => 'El grupo debe ser A o B',
             'centro_id.required' => 'El entrenador es obligatorio.',
             'centro_id.exists' => 'El entrenador seleccionado no es válido.',
+            'jugadores.*.equipo_id.required' => 'El id del equipo es obligatorio.',
+            'jugadores.*.equipo_id.int' => 'El id del equipo tiene que ser un entero.',
+            'jugadores.*.nombre_completo.required' => 'El nombre completo es obligatorio.',
+            'jugadores.*.nombre_completo.string' => 'El nombre completo debe ser una cadena de texto.',
+            'jugadores.*.nombre_completo.min' => 'El nombre completo debe tener al menos 3 caracteres.',
+            'jugadores.*.nombre_completo.max' => 'El nombre completo no puede superar los 70 caracteres.',
+            'jugadores.*.capitan.required' => 'El campo capitán es obligatorio.',
+            'jugadores.*.capitan.int' => 'El campo capitán debe ser un entero.',
+            'jugadores.*.estudio_id.int' => 'El id del estudio debe ser un entero.',
+            'jugadores.*.dni.string' => 'El DNI debe ser una cadena de texto.',
+            'jugadores.*.dni.min' => 'El DNI debe tener exactamente 9 caracteres.',
+            'jugadores.*.dni.max' => 'El DNI debe tener exactamente 9 caracteres.',
+            'jugadores.*.dni.digits' => 'El DNI debe contener exactamente 8 dígitos.',
+            'jugadores.*.dni.unique' => 'El DNI ya está registrado.',
+            'jugadores.*.email.string' => 'El email debe ser una cadena de texto.',
+            'jugadores.*.email.email' => 'El email debe ser una dirección de correo válida.',
+            'jugadores.*.email.unique' => 'El email ya está registrado.',
+            'jugadores.*.telefono.string' => 'El teléfono debe ser una cadena de texto.',
+            'jugadores.*.telefono.min' => 'El teléfono debe tener exactamente 9 caracteres.',
+            'jugadores.*.telefono.max' => 'El teléfono debe tener exactamente 9 caracteres.',
         ];
     }
 }
