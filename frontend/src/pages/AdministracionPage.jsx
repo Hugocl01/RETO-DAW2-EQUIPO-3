8d4624c65bff878cbb9d05afaf6d13e0a9bdd175
Hugocl01 <hugocayon@gmail.com>
2025-02-16 17:27:33 +0100

    Gestionar un parametos de la url en adminastracion Fixes #218

--- frontend/src/pages/AdministracionPage.jsx
+++ frontend/src/pages/AdministracionPage.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect, useContext } from "react";
+import { useLocation } from "react-router-dom";
 import { SeguridadContext } from "../contexts/SeguridadProvider";
 import api from "../services/api";
 import AdministracionMenu from "../components/MenuAdministracion";
@@ -9,12 +10,24 @@ function AdministracionPage() {
     const [loading, setLoading] = useState(true);
     const [entidadSeleccionada, setEntidadSeleccionada] = useState(null);
     const { seguridad } = useContext(SeguridadContext);
+    const location = useLocation();
 
     useEffect(() => {
         const fetchSecciones = async () => {
             try {
                 const response = await api.get(`/perfiles/${seguridad.user.perfil.id}`);
-                setSecciones(response.data.perfiles.secciones || []);
+                const seccionesData = response.data.perfiles.secciones || [];
+                setSecciones(seccionesData);
+
+                // Obtener el primer parámetro de la URL
+                const params = new URLSearchParams(location.search);
+                for (const [key] of params.entries()) {
+                    const seccionEncontrada = seccionesData.find(s => s.nombre.toLowerCase() === key.toLowerCase());
+                    if (seccionEncontrada) {
+                        setEntidadSeleccionada(seccionEncontrada);
+                        break; // Salimos del bucle al encontrar la primera coincidencia
+                    }
+                }
             } catch (error) {
                 console.error("Error al obtener secciones:", error);
             } finally {
@@ -23,14 +36,15 @@ function AdministracionPage() {
         };
 
         fetchSecciones();
-    }, []);
+    }, [location.search]); // Se ejecuta cuando cambia la URL
 
     const handleLoad = (seccion) => {
         setEntidadSeleccionada(seccion);
-    }
+    };
+
     return (
         <div className="d-flex">
-            <title>Adminstración</title>
+            <title>Administración</title>
             <AdministracionMenu secciones={secciones} loading={loading} onSelect={handleLoad} />
 
             <div className="flex-grow-1 p-4">
