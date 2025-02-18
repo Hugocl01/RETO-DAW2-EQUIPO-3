a26aaaa0db3ebca8959ce5e6162e00dde8b873b4
Hugocl01 <hugocayon@gmail.com>
2025-02-18 19:31:59 +0100

    correccion

--- frontend/src/data/CamposCiclosFormulario.js
+++ frontend/src/data/CamposCiclosFormulario.js
@@ -1,5 +1,31 @@
+import api from "../services/api";
+
+const fetchFamilias = async () => {
+    try {
+        const response = await api.get("/familias");
+        return response.data.estudios.map(familia => ({
+            value: familia.id,
+            label: familia.nombre
+        }));
+    } catch (error) {
+        console.error("Error al obtener los familias", error);
+        return [];
+    }
+};
+
 const campoCiclosFormulario = [
-    
+    {
+        name: 'nombre',
+        label: 'Nombre',
+        type: 'text',
+        placeholder: 'Ingrese el nombre'
+    },
+    {
+        name: 'familia_id',
+        label: 'Familia',
+        type: 'select',
+        options: fetchFamilias()
+    }
 ];
 
 export default campoCiclosFormulario;
