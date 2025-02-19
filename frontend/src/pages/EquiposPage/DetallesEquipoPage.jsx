c1a18195a1f64ed153c3aad44d0e558435626e22
adriantresgallo <adrian3gallo@gmail.com>
2025-02-19 12:59:44 +0100

    Estilo páginas equipos Fixes #265

--- frontend/src/pages/EquiposPage/DetallesEquipoPage.jsx
+++ frontend/src/pages/EquiposPage/DetallesEquipoPage.jsx
@@ -1,41 +1,21 @@
 import React, { useEffect, useState } from "react";
-import { useLocation, useNavigate, useParams } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import api from "../../services/api.js";
 import Spinner from "../../components/Spinner.jsx";
-
-/**
- * Página de Detalles del Equipo
- * @returns
- */
+import "../../core/CSS/DetalleEquipoPage.css";
 
 function DetallesEquipoPage() {
-  /**
-   * Con esto, obtengo la ruta con el location
-   */
   const location = useLocation();
-
-  /**
-   * Estado para el equipo a mostrar
-   */
   const [equipo, setEquipo] = useState();
-
   const [cargando, setCargando] = useState(true);
-
   const navegar = useNavigate();
 
-  /**
-   * Se ejecutará cuando haya cambios en el nombre que pasemos en el header
-   */
   useEffect(() => {
     const obtenerEquipo = async () => {
       try {
-        /**
-         * Implemento el location para que me devuelva la ruta en la que nos encontramos
-         */
         const resultado = await api.get(location.pathname);
         if (resultado.data.status === "success") {
           setCargando(false);
-          console.log(resultado.data.equipo);
           setEquipo(resultado.data.equipo);
         }
       } catch (error) {
@@ -46,69 +26,86 @@ function DetallesEquipoPage() {
   }, [location]);
 
   if (cargando) {
-    return <Spinner></Spinner>;
+    return <Spinner />;
   }
 
-  /**
-  * Función que envuelve le useNavigate y que me sirve para navegar a la página de detalles del jugador
-  */
   function navegarDetalleJugador(slug) {
-    navegar(`/jugadores/${slug}`)
+    navegar(`/jugadores/${slug}`);
   }
+
   return (
     <>
       <title>Detalles del Equipo</title>
-      <section className="container-fluid">
+      <section className="container-fluid py-5">
         <div className="row">
-          {/**Sección de info de equipo */}
-          <section id="infoEquipo" className="col-md-4">
-            <h2>Equipo {equipo.nombre}</h2>
-
-            <div>
-              <p>Entrenador: {equipo.entrenador} </p>
-            </div>
-
-            <div>
-              <img
-                src={"../../src/assets/imagenes/img1.jpg"}
-                alt="imagenPatrocinador"
-                className="img-fluid"
-                style={{ width: "50%" }}
-              />
-            </div>
-
-            <div>
-              <p>Centro: {equipo.centro}</p>
-              <p>Grupo: {equipo.grupo}</p>
+          {/* Sección de información del equipo */}
+          <section id="infoEquipo" className="col-md-4 p-0">
+            <div className="card shadow-sm border-light rounded">
+              <div className="card-body text-center">
+                <h2 className="card-title mb-3 w-100 bg-primary text-white py-2 rounded-3">
+                  Equipo {equipo.nombre}
+                </h2>
+                <div className="mb-3">
+                  <img
+                    src={"../../src/assets/imagenes/img1.jpg"} // Aquí podrías usar una imagen específica para el equipo
+                    alt="imagenPatrocinador"
+                    className="img-fluid"
+                    style={{ width: "70%" }}
+                  />
+                </div>
+                <div className="d-flex flex-column justify-content-center align-items-center text-justify">
+                  <p>
+                    <strong>Entrenador:</strong> {equipo.entrenador}
+                  </p>
+                  <p>
+                    <strong>Centro:</strong> {equipo.centro}
+                  </p>
+                  <p>
+                    <strong>Grupo:</strong> {equipo.grupo}
+                  </p>
+                </div>
+              </div>
             </div>
           </section>
 
           {/* Sección de jugadores */}
-          <section id="jugadores" className="col-md-8">
-            <div className="row">
-              <div className="col-12">
-                <h3 className="text-center mb-4">Jugadores</h3>
-                <div className="row">
+          <section id="jugadores" className="col-md-8 ">
+            <div className="card shadow-sm border-light rounded">
+                <div className="card-body text-center">
+                  <h2 className="card-title mb-3 w-100 bg-primary text-white py-2 rounded-3">
+                    Jugadores
+                  </h2>
+                </div>
+                <div className="row d-flex flex-row flex-wrap justify-content-center align-items-center g-4">
                   {equipo.Jugadores.jugador.map((valor, index) => (
-                    <div key={index} className="col-md-3 mb-2">
-                      <div className="card h-100" onClick={() => navegarDetalleJugador(valor.slug)}>
-                        <img
-                          src={"../../src/assets/imagenes/img1.jpg"}
-                          alt="imagenJugador"
-                          className="card-img-top img-fluid"
-                          style={{ objectFit: "cover", height: "75%" }}
-                        />
-                        <div className="card-body d-flex flex-column">
-                          <p className="card-text mt-auto text-center">
-                            {valor.nombre}
-                          </p>
+                    <div
+                      key={index}
+                      className="col-12 col-sm-6 col-md-3 mb-4 d-flex justify-content-center align-items-center"
+                      onClick={() => navegarDetalleJugador(valor.slug)}
+                    >
+                      <div className="flip-card shadow-sm">
+                        <div className="flip-card-inner">
+                          {/* Parte frontal de la carta (imagen) */}
+                          <div className="flip-card-front">
+                            <img
+                              src={`../../src/assets/imagenes/messi.jpg`} // Cambiar para cada jugador
+                              alt="imagenJugador"
+                              className="card-img-top img-fluid rounded"
+                            />
+                          </div>
+                          {/* Parte posterior de la carta (nombre del jugador) */}
+                          <div className="flip-card-back d-flex justify-content-center align-items-center">
+                            <p>
+                              <strong>{valor.nombre}</strong>
+                            </p>
+                          </div>
                         </div>
                       </div>
                     </div>
                   ))}
                 </div>
               </div>
-            </div>
+            
           </section>
         </div>
       </section>
