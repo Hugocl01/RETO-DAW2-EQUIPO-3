977e4bc9509afa7c993bab73e6eaf6b4732bad2e
paularivero22 <paulariveroherreros789@gmail.com>
2025-02-22 11:51:47 +0100

    modal iniciar sesion y mejoras en el sidebar

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,21 +3,33 @@ import { Link } from "react-router-dom";
 import { SeguridadContext } from "../contexts/SeguridadProvider";
 import YouTubeLiveIndicator from "./YouTubeLiveIndicator";
 import { useLocation } from "react-router-dom";
-
 import "./css/Header.css";
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import LoginModal from './LoginModal';
 
 function Header() {
   const { seguridad, logout } = useContext(SeguridadContext);
   const location = useLocation();
 
   const [isSidenavActive, setIsSidenavActive] = useState(false); // Estado para manejar el toggle del sidenav
-
   const isActive = (path) => (location.pathname === path ? "active" : "");
+
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   // Función para manejar el toggle del sidenav
   const toggleSidenav = () => {
     setIsSidenavActive(prevState => !prevState);
   };
+
+  const [isSubMenuActive, setIsSubMenuActive] = useState(false);
+
+  const toggleSubMenu = () => {
+    setIsSubMenuActive(!isSubMenuActive);
+  };
+
 
   return (
     <header className="p-3">
@@ -33,86 +45,74 @@ function Header() {
           {/* Barra de navegación (centrada) */}
           <div className="d-flex align-items-center justify-content-center flex-grow-1" id="menu">
             <ul className="nav col-13 col-lg-auto me-lg-auto mb-3 d-flex justify-content-center align-items-center mb-md-0">
-              <li>
-                <Link to="/" className={`nav-link px-2 link-body-emphasis ${isActive("/")}`} id="elementoMenu">
-                  INICIO
-                </Link>
-              </li>
-
-              <li>
-                <Link to="/equipos" className={`nav-link px-2 link-body-emphasis ${isActive("/")}`} id="elementoMenu">
-                  EQUIPOS
-                </Link>
-              </li>
-
-              <li className="nav-item dropdown">
-                <Link to="/partidos" className={`nav-link px-2 link-body-emphasis ${isActive("/partidos")}`} id="elementoMenu" data-bs-toggle="dropdown" role="button" aria-expanded="false">
-                  TORNEO
-                </Link>
-                <ul className="dropdown-menu">
-                  <li>
-                    <Link to="/partidos" className="dropdown-item">
-                      RESULTADOS
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to="/clasificacion" className="dropdown-item">
-                      CLASIFICACIÓN
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to="/estadisticas" className="dropdown-item">
-                      ESTADÍSTICAS
-                    </Link>
-                  </li>
-                </ul>
-              </li>
-
-              <li>
-                <Link to="/organizacion" className={`nav-link px-2 link-body-emphasis ${isActive("/organizacion")}`} id="elementoMenu">
-                  ORGANIZACIÓN
-                </Link>
-              </li>
-              <li>
-                <Link to="/galeria" className={`nav-link px-2 link-body-emphasis ${isActive("/galeria")}`} id="elementoMenu">
-                  GALERÍA
-                </Link>
-              </li>
-              <li>
-                <Link to="/inscribirse" className={`nav-link px-2 link-body-emphasis ${isActive("/inscribirse")}`} id="elementoMenu">
-                  INSCRIBIRSE
-                </Link>
-              </li>
-              <li className="d-flex align-items-center">
-                <YouTubeLiveIndicator />
-              </li>
+              {/* Your menu items here */}
             </ul>
           </div>
 
-          {/* Botón de iniciar sesión (alineado a la derecha) */}
+          {/* Button to trigger modal */}
           <div className="flex-grow-0" id="iniciarSesion">
             {seguridad.user ? (
               <div className="dropdown text-end">
-                <a href="#" className="d-block link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                  <img src="https://github.com/mdo.png" alt="Perfil de usuario" width="32" height="32" className="rounded-circle" />
+                <a
+                  href="#"
+                  className="d-block link-body-emphasis text-decoration-none dropdown-toggle"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  <img
+                    src="https://github.com/mdo.png"
+                    alt="Perfil de usuario"
+                    width="32"
+                    height="32"
+                    className="rounded-circle"
+                  />
                 </a>
                 <ul className="dropdown-menu text-small">
                   <li>
                     <h2 className="dropdown-header">{seguridad.user.name}</h2>
                   </li>
-                  <li><hr className="dropdown-divider" /></li>
-                  <li><Link to="/administracion" className="dropdown-item">Administración</Link></li>
-                  <li><Link to="/perfil" className="dropdown-item">Perfil</Link></li>
-                  <li><hr className="dropdown-divider" /></li>
-                  <li><a className="dropdown-item" href="" onClick={logout}>Cerrar sesión</a></li>
+                  <li>
+                    <hr className="dropdown-divider" />
+                  </li>
+                  <li>
+                    <Link to="/administracion" className="dropdown-item">
+                      Administración
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/perfil" className="dropdown-item">
+                      Perfil
+                    </Link>
+                  </li>
+                  <li>
+                    <hr className="dropdown-divider" />
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="" onClick={logout}>
+                      Cerrar sesión
+                    </a>
+                  </li>
                 </ul>
               </div>
             ) : (
-              <Link to="/login" className="btn btn-primary text-black ms-2 text-white">
+              <button className="btn btn-primary text-white" onClick={handleShow}>
                 Iniciar sesión
-              </Link>
+              </button>
             )}
           </div>
+          <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>Iniciar sesión</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <LoginModal handleClose={handleClose} />
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Cerrar
+              </Button>
+            </Modal.Footer>
+          </Modal>
         </div>
       </div>
 
@@ -138,25 +138,19 @@ function Header() {
                 </Link>
               </li>
 
-              <li className="nav-item dropdown">
-                <Link to="/partidos" className={`nav-link px-2 link-body-emphasis ${isActive("/partidos")}`} id="elementoMenu" data-bs-toggle="dropdown" role="button" aria-expanded="false">
+              <li className="nav-item">
+                <Link to="#" className={`nav-link px-2 link-body-emphasis ${isActive("/torneo")}`} id="elementoMenu" onClick={toggleSubMenu}>
                   TORNEO
                 </Link>
-                <ul className="dropdown-menu">
+                <ul className={`submenu ${isSubMenuActive ? "active" : ""}`} style={{ maxHeight: isSubMenuActive ? "500px" : "0", opacity: isSubMenuActive ? "1" : "0" }}>
                   <li>
-                    <Link to="/partidos" className="dropdown-item">
-                      RESULTADOS
-                    </Link>
+                    <Link to="/partidos" className="nav-link px-2 link-body-emphasis">RESULTADOS</Link>
                   </li>
                   <li>
-                    <Link to="/clasificacion" className="dropdown-item">
-                      CLASIFICACIÓN
-                    </Link>
+                    <Link to="/clasificacion" className="nav-link px-2 link-body-emphasis">CLASIFICACIÓN</Link>
                   </li>
                   <li>
-                    <Link to="/estadisticas" className="dropdown-item">
-                      ESTADÍSTICAS
-                    </Link>
+                    <Link to="/estadisticas" className="nav-link px-2 link-body-emphasis">ESTADÍSTICAS</Link>
                   </li>
                 </ul>
               </li>
@@ -180,6 +174,29 @@ function Header() {
                 <YouTubeLiveIndicator />
               </li>
             </ul>
+            <div className="flex-grow-0 px-4" id="iniciarSesion">
+              {seguridad.user ? (
+                <div className="dropdown text-start">
+                  <a href="#" className="d-block link-body-emphasis text-decoration-none dropdown-toggle ms-2" data-bs-toggle="dropdown" aria-expanded="false">
+                    <img src="https://github.com/mdo.png" alt="Perfil de usuario" width="32" height="32" className="rounded-circle" />
+                  </a>
+                  <ul className="dropdown-menu text-small">
+                    <li>
+                      <h2 className="dropdown-header">{seguridad.user.name}</h2>
+                    </li>
+                    <li><hr className="dropdown-divider" /></li>
+                    <li><Link to="/administracion" className="dropdown-item">Administración</Link></li>
+                    <li><Link to="/perfil" className="dropdown-item">Perfil</Link></li>
+                    <li><hr className="dropdown-divider" /></li>
+                    <li><a className="dropdown-item" href="" onClick={logout}>Cerrar sesión</a></li>
+                  </ul>
+                </div>
+              ) : (
+                <Link to="/login" className="btn btn-primary text-black ms-2 text-white">
+                  Iniciar sesión
+                </Link>
+              )}
+            </div>
           </nav>
           {/* boton para mostrar el sidenav */}
           <button
@@ -191,7 +208,7 @@ function Header() {
           </button>
         </div>
       </div>
-    </header >
+    </header>
   );
 }
 
