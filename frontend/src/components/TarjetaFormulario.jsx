2dc968e02c60780c7f5402beb7078594be33d4d9
paularivero22 <paulariveroherreros789@gmail.com>
2025-02-27 14:54:14 +0100

    cambio del formulario de inscripcion

---
+++ frontend/src/components/TarjetaFormulario.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import "./css/TarjetaFormulario.css";
+
+function TarjetaFormulario({ titulo, id, children }) {
+    const [abiertas, setAbiertas] = useState({});
+
+    useEffect(() => {
+        setAbiertas((prev) => ({
+            ...prev,
+            [id]: true,
+        }));
+    }, [id]);
+
+    const toggleTarjeta = () => {
+        setAbiertas((prev) => ({
+            ...prev,
+            [id]: !prev[id],
+        }));
+    };
+
+    return (
+        <div className="card mb-3">
+            <div
+                className="card-header bg-primary text-white"
+                onClick={toggleTarjeta}
+                style={{ cursor: "pointer" }}
+            >
+                {titulo}
+            </div>
+            {abiertas[id] && <div className="card-body">{children}</div>}
+        </div>
+    );
+}
+
+export default TarjetaFormulario;
