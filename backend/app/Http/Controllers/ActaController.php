37750c6b43dbe18f7c01321d5717c6e322a6de9d
Etsiu7 <eloycuesta@hotmail.es>
2025-03-04 16:56:02 +0100

    ActaController store show delete

--- backend/app/Http/Controllers/ActaController.php
+++ backend/app/Http/Controllers/ActaController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\ActaRequest;
+use App\Http\Resources\ActaResource;
+use App\Models\Acta;
 
 class ActaController extends Controller
 {
@@ -10,29 +13,39 @@ class ActaController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(ActaRequest $request)
     {
-        //
+        $acta = Acta::create($request->validated());
+
+        return response()->json([
+            'status'  => 'success',
+            'message' => 'Acta creada correctamente',
+            'acta' => new ActaResource($acta),
+        ], 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id) {}
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
+    public function show(Acta $acta)
     {
-        //
+        return response()->json([
+            'status'  => 'success',
+            'message' => 'Detalle del Acta',
+            'acta' => new ActaResource($acta),
+        ], 200);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Acta $acta)
     {
-        //
+        $acta->delete();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Acta eliminada exitosamente'
+        ]);
     }
 }
