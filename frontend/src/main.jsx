bb678ac0da0926b542b4a7692abb53ed77e8c8ce
Hugocl01 <hugocayon@gmail.com>
2025-03-05 22:12:44 +0100

    Documentacion jsdoc

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,9 +11,24 @@ import "./index.css";
 import { SeguridadProvider } from './contexts/SeguridadProvider.jsx';
 import AppEnrutador from './routers/AppEnrutador.jsx';
 
+/**
+ * Punto de entrada principal de la aplicación.
+ * Este archivo es responsable de inicializar la aplicación de React y montar el árbol de componentes en el DOM.
+ * 
+ * - Se aplica el `SeguridadProvider` para que todos los componentes hijos puedan acceder a los datos de seguridad/autenticación.
+ * - Se importa y aplica Bootstrap y Bootstrap Icons para los estilos y la funcionalidad de la interfaz de usuario.
+ * - Se utiliza `StrictMode` para activar advertencias sobre el ciclo de vida de los componentes y posibles problemas.
+ * 
+ * @function
+ * @example
+ * // Este archivo se ejecuta al iniciar la aplicación.
+ * // No es necesario invocar una función manualmente, ya que es el punto de entrada.
+ */
 createRoot(document.getElementById('root')).render(
     <StrictMode>
+        {/* Proveedor de seguridad que envuelve la aplicación */}
         <SeguridadProvider>
+            {/* Componente de enrutamiento de la aplicación */}
             <AppEnrutador />
         </SeguridadProvider>
     </StrictMode>,
