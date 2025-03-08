7ce2e8400dd5cf2374f2be68379ff5309dc1b7c8
Hugocl01 <hugocayon@gmail.com>
2025-03-08 16:56:46 +0100

    correccion swagger

--- backend/app/Models/Publicacion.php
+++ backend/app/Models/Publicacion.php
@@ -6,6 +6,24 @@ use App\Traits\Auditable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @OA\Schema(
+ *     schema="Publicacion",
+ *     type="object",
+ *     required={"id", "titulo", "contenido", "publicacionable_id", "publicacionable_type"},
+ *     @OA\Property(property="id", type="integer", description="ID de la publicación"),
+ *     @OA\Property(property="titulo", type="string", description="Título de la publicación"),
+ *     @OA\Property(property="contenido", type="string", description="Contenido de la publicación"),
+ *     @OA\Property(property="portada", type="string", description="Indica si la publicación tiene portada (Si/No)"),
+ *     @OA\Property(property="publicacionable_type", type="string", description="Tipo de modelo relacionado con la publicación (Equipo, Partido, etc.)"),
+ *     @OA\Property(property="publicacionable_id", type="integer", description="ID del modelo relacionado con la publicación"),
+ *     @OA\Property(
+ *         property="imagenes",
+ *         type="array",
+ *         @OA\Items(type="string", description="URLs de las imágenes relacionadas con la publicación")
+ *     )
+ * )
+ */
 class Publicacion extends Model
 {
     use Auditable, HasFactory;
