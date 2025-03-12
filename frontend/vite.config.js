b3a5073195a99bf89a4eaa8cb6379c75e0f676c1
Hugocl01 <hugocayon@gmail.com>
2025-03-12 12:03:34 +0100

    cambios

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+const apiUrl = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8000',
+        target: apiUrl,
         changeOrigin: true,
         secure: false,
       },
